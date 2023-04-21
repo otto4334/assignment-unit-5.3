5989d4dd0ad4607d88e71c7d39a60b6af010efe2
Michael Otto <otto4334@gmail.com>
2023-04-21 17:38:20 -0500

    Added more albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,5 +10,35 @@ const gorillaz = {
     yearPublished: 2001
 }
 
+const clarity = {
+    title: 'Zedd',
+    artist: 'Clarity',
+    yearPublished: 2012
+}
+
+const theMollusk = {
+    title: 'The Mollusk',
+    artist: 'Ween',
+    yearPublished: 1997
+}
+
+const siameseDream = {
+    title: 'Siamese Dream',
+    artist: 'Smashing Pumpkins',
+    yearPublished: 1993
+}
+
+const cowboyBebop = {
+    title: 'COWBOY BEBOP',
+    artist: 'Seatbelts',
+    yearPublished: 1998
+}
+
+const moment = {
+    title: 'moment',
+    artist: 'Vierre Cloud',
+    yearPublished: 2019
+}
+
 addToCollection(gorillaz)
 console.log(collection)
