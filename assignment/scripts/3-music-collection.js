a2e7ae25f927af9ed14fc976fc0bc16bfe869298
Michael Otto <otto4334@gmail.com>
2023-04-20 21:29:23 -0500

    added addtoCollection function and added 1 album object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,14 @@
 console.log('***** Music Collection *****')
 let collection = []
+function addToCollection (title, artist, yearPublished){
+    return collection.push(title, artist, yearPublished);
+}
+
+const gorillaz = {
+    title: 'Gorillaz',
+    artist: 'Gorillaz',
+    yearPublished: 2001
+}
+
+addToCollection(gorillaz)
 console.log(collection)
